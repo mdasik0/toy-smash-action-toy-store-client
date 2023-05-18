8bf751b582ce9d476fa1c614789b266697ca6318
mdasik0 <asikthe1st@gmail.com>
2023-05-18 18:48:59 +0600

    first commit

---
+++ src/routes/Route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello asik!</div>,
+    },
+  ]);
+
+export default router;
