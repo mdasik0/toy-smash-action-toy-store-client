246149e9d7832dc59718c6255ec87895ffb9b806
mdasik0 <asikthe1st@gmail.com>
2023-05-22 00:05:15 +0600

    readme file added

--- src/components/Pages/HomePage/Extraone/Extraone.jsx
+++ src/components/Pages/HomePage/Extraone/Extraone.jsx
@@ -1,8 +1,9 @@
 const Extraone = () => {
   return (
     <div>
-      <div className="bg-slate-400 md:w-[1100px] h-[500px] mb-[12rem] w-full md:mx-auto mx-3 relative">
-        <h1 className="text-center hover:-translate-y-2 hover:translate-x-3 duration-500 text-5xl font-bold pt-10">Today&apos;s letest <span className="text-yellow-500">Sale</span></h1>
+        
+      <div  className="bg-slate-400 md:w-[1100px] h-[500px] mb-[12rem] w-full md:mx-auto mx-3 relative">
+        <h1 data-aos="fade-right" className="text-center hover:-translate-y-2 hover:translate-x-3 duration-500 text-5xl font-bold pt-10">Today&apos;s letest <span className="text-yellow-500">Sale</span></h1>
         <p className="text-center w-1/2 mx-auto md:mt-6 mt-3" >every day we realease a one item.with half of it&apos;s price.Grab your chance and Get it !!!</p>
         <div className="absolute md:flex-row flex-col left-20 -bottom-36 md:left-64 flex bg-slate-200">
             <div className="md:w-[400px] md:hover:w-[420px] duration-700 hover:h-[420px] h-[400px]">
--- src/components/Pages/HomePage/Extratwo/Extratwo.jsx
+++ src/components/Pages/HomePage/Extratwo/Extratwo.jsx
@@ -3,54 +3,64 @@ const Extratwo = () => {
     <div className="md:w-[1280px] md:my-10 my-3 md:mx-auto mx-3 w-full grid gap-10 md:grid-cols-3 grid-cols-1  ">
       <div className="card rounded-lg w-96 bg-base-100 shadow-xl image-full">
         <figure>
-          <img
-            src="/src/assets/assets/img_14.jpg"
-            alt="Key Chain"
-          />
+          <img src="/src/assets/assets/img_14.jpg" alt="Key Chain" />
         </figure>
         <div className="card-body p-16">
           <h2 className="card-title">Key Chain!</h2>
           <p>DeadPool key chains </p>
           <div className="badge badge-accent">30% off</div>
-          <div className=""><span className="line-through">$45</span> now at <span className="text-3xl font-bold">$30 !!!</span></div>
+          <div className="">
+            <span className="line-through">$45</span> now at{" "}
+            <span className="text-3xl font-bold">$30 !!!</span>
+          </div>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
       </div>
-      <div className="card rounded-lg w-96 bg-base-100 shadow-xl image-full">
-        <figure>
-          <img
-          className=""
-            src="/src/assets/assets/img_12.jpg"
-            alt="Key Chain"
-          />
-        </figure>
-        <div className="card-body p-16">
-          <h2 className="card-title">Iron man doll!</h2>
-          <p>Iron Man Golden doll</p>
-          <div className="badge badge-accent">45% off</div>
-          <div className=""><span className="line-through">$99</span> now at <span className="text-3xl font-bold">$53 !!!</span></div>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+      <div data-aos="zoom-in">
+        <div className="card rounded-lg w-96 bg-base-100 shadow-xl image-full">
+          <figure>
+            <img
+              className=""
+              src="/src/assets/assets/img_12.jpg"
+              alt="Key Chain"
+            />
+          </figure>
+          <div className="card-body p-16">
+            <h2 className="card-title">Iron man doll!</h2>
+            <p>Iron Man Golden doll</p>
+            <div className="badge badge-accent">45% off</div>
+            <div className="">
+              <span className="line-through">$99</span> now at{" "}
+              <span className="text-3xl font-bold">$53 !!!</span>
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="card rounded-lg w-96 bg-base-100 shadow-xl image-full">
-        <figure>
-          <img
-          className=""
-            src="/src/assets/assets/img_24.jpg"
-            alt="Key Chain"
-          />
-        </figure>
-        <div className="card-body p-16">
-          <h2 className="card-title">Yoda plusshi!</h2>
-          <p>Yoda Plussi made with 100% Cotton</p>
-          <div className="badge text-xl badge-accent">50% off</div>
-          <div className=""><span className="line-through">$199</span> now at <span className="text-3xl font-bold">$99 !!!</span></div>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+      <div data-aos="zoom-in">
+        <div className="card rounded-lg w-96 bg-base-100 shadow-xl image-full">
+          <figure>
+            <img
+              className=""
+              src="/src/assets/assets/img_24.jpg"
+              alt="Key Chain"
+            />
+          </figure>
+          <div className="card-body p-16">
+            <h2 className="card-title">Yoda plusshi!</h2>
+            <p>Yoda Plussi made with 100% Cotton</p>
+            <div className="badge text-xl badge-accent">50% off</div>
+            <div className="">
+              <span className="line-through">$199</span> now at{" "}
+              <span className="text-3xl font-bold">$99 !!!</span>
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
         </div>
       </div>
